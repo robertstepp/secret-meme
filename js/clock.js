c15943a54a66a351ea8cb12dd123cf3cbeaf479d
Robert <robert@vyperinc.ninja>
2014-08-21 00:44:30 -0400

    Fixed clock and added Zulu

--- js/clock.js
+++ js/clock.js
@@ -3,14 +3,18 @@ d = new Date();
 nhour  = d.getHours();
 nmin   = d.getMinutes();
 nsec   = d.getSeconds();
-ntz    = d.toString().match(/\(([A-Za-z\s].*)\)/)[1];
+//ntz    = d.toString().match(/\(([A-Za-z\s].*)\)/)[1];
 if(nhour <= 9) {nhour = "0" +nhour;}
 if(nmin <= 9) {nmin = "0" +nmin;}
 if(nsec <= 9) {nsec = "0" +nsec;}
+uhour  = d.getUTCHours();
+umin   = d.getUTCMinutes();
+usec   = d.getUTCSeconds();
+if(uhour <= 9) {uhour = "0" +uhour;}
+if(umin <= 9) {umin = "0" +umin;}
+if(usec <= 9) {usec = "0" +usec;}
 
-
-
-document.getElementById('clockbox').innerHTML=""+nhour+":"+nmin+":"+nsec+" "+ntz+"";
+document.getElementById('clockbox').innerHTML=""+nhour+":"+nmin+":"+nsec+"L | "+uhour+":"+umin+":"+usec+"Z";
 setTimeout("GetClock()", 1000);
 }
 window.onload=GetClock;
