b106c23e5cd4434fd3905103eddaf3e39a6bd529
Robert <robert@vyperinc.ninja>
2014-08-25 13:57:15 -0400

    add userid to bookmarks and settings tables

--- add_bookmarks.php
+++ add_bookmarks.php
@@ -3,7 +3,7 @@ require 'session.php'; // Check if Session is set //
 // Page Title //
 $title="Add Bookmarks";
 // Page Description //
-$Description="Add bookmakrs to your account";
+$Description="Add bookmarks to your account";
 require 'db_connect.php';
 require 'head.php';
 require 'addbookmarks.php';
