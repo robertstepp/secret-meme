9d7f38e30f7bd0ab0c6d9382814217a16a72ccef
Robert <robert@vyperinc.ninja>
2014-08-25 14:39:16 -0400

    Added add bookmarks page

---
+++ add.php
@@ -0,0 +1,27 @@
+<?php
+require 'session.php';
+require 'db_connect.php';
+$uid		= $_SESSION['sess_user_id'];
+$type 		= $_POST['type'];
+$groupid 	= $_POST['groupid'];
+$title		= $_POST['title'];
+$url		= $_POST['url'];
+$favicon	= $_POST['favicon'];
+$nsfw		= $_POST['nsfw'];
+$home		= $_POST['home'];
+
+echo $uid . $type . $groupid . $title . $url . $favicon . $nsfw . $home;
+if (isset($uid,$type,$groupid,$title,$url,$favicon,$nsfw,$home))	{
+	$sql = "INSERT INTO bookmarks (uid,type,groupid,title,url,favicon,nsfw,home) VALUES ('".$uid."','".$type."','".$groupid."','".$title."','".$url."','".$favicon."','".$nsfw."','".$home."')";
+	$res = mysqli_query($con,$sql);
+	if ($res)  {
+		echo $title . ' added.';
+		sleep(10);
+		header('Location: ' . $_SERVER['HTTP_REFERER']);		
+	}	else  {
+		echo $title . ' failed to add.';
+		sleep(10);
+		header('Location: ' . $_SERVER['HTTP_REFERER']);
+	} 
+}
+?>
--- addbookmarks.php
+++ addbookmarks.php
@@ -8,16 +8,48 @@ echo '
 		<tr>
 			<td>Type:</td>
 			<td>
-				<select>';
-				$type = mysqli_query($con,"SELECT 'type','uid' FROM `bookmarks`.`bookmarks` WHERE uid='" . $_SESSION['sess_user_id'] . "' GROUP BY `type`");
+				<select name="type" id="type">
+					';
+				$type = mysqli_query($con,"SELECT type FROM bookmarks WHERE uid=" . $_SESSION['sess_user_id'] . " GROUP BY type");
 				while($types = mysqli_fetch_array($type))	{
-					echo "
-					<option value='".$types."'>".$types."</option>
+					echo "<option value='".$types['type']."'>" . $types['type'] . "</option>
 					";
 				}
 			echo '
 				</select>
 				</td>
 		</tr>
+		<tr>
+			<td>Group:</td>
+			<td><input type="text" name="groupid" id="groupid" /></td>
+		</tr>
+		<tr>
+			<td>Title:</td>
+			<td><input type="text" name="title" id="title" /></td>
+		</tr>
+		<tr>
+			<td>URL:</td>
+			<td><input type="url" name="url" id="url" /></td>
+		</tr>
+		<tr>
+			<td>Favicon(if known):</td>
+			<td><input type="url" name="favicon" id="favicon" /></td>
+		</tr>
+		<tr>
+			<td>NSFW:</td>
+			<input type="hidden" value="0" name="nsfw" />
+			<td><input type="checkbox" name="nsfw" value="1" /></td>
+		</tr>
+		<tr>
+			<td>Home Screen:</td>
+			<input type="hidden" value="0" name="home" />
+			<td><input type="checkbox" name="home" value="1" /></td>
+		</tr>
+		<tr>
+			<td>&nbsp;</td>
+			<td>
+				<input type="submit" name="button" id="button" value="Submit" />
+				<input type="reset" name="button2" id="button2" value="Oops!" />
+			</td>
 	</table>
 </form>';
