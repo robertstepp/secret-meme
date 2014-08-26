4cf40c36ebc751f40aebd0b27fc9c77612bfe4c5
Robert <robert@vyperinc.ninja>
2014-08-26 11:12:14 -0400

    Added javascript to pull screen size and use it in style for HTML

--- js/screensize.js
+++
@@ -1,7 +0,0 @@
-function screensize() {
-	<!--
-		document.location=\"$PHP_SELF?resolution=1&width=\"+window.innerWidth+\"&height=\"+window.innerHeight;
-		//--> 
-};
-window.onload = screensize();
-$(window).resize(screensize());
