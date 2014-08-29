7cd0e48247ee234b8eeb51fa3e9745c26917860e
Robert <robert@vyperinc.ninja>
2014-08-29 09:43:54 -0400

    updated links

--- forms/links.php
+++ forms/links.php
@@ -1,19 +1,21 @@
 <?php
+require 'db_connect.php';
 echo '
 <form id="link_post" name="link_post" method="post" action="../posts/linkpost.php">
 <table id="links">
 	<tr>
 		<th colspan="2">Link to other pages</th>
-	</tr>
+	</tr>';
+$types = mysqli_query($con,"SELECT type,uid FROM bookmarks WHERE uid='" . $_SESSION['sess_user_id'] . "' GROUP BY type ASC");
+while($type = mysqli_fetch_array($types))	{
+	echo '
 	<tr>
-		<td>Work Bookmarks</td>
-		<td><input type="radio" value="Work" name="type" /></td>
+		<td>' . $type['type'] . ' Bookmarks</td>
+		<td><input type="radio" name="type" value="' . $type['type'] . '" /></td>
 	</tr>
-	<tr>
-		<td>Personal Bookmarks</td>
-		<td><input type="radio" value="Personal" name="type" /></td>
-	</tr>
-	<tr>
+	';
+}
+echo '		<tr>
 		<td>Include NSFW</td>
 			<input type="hidden" value="0" name="nsfw" />
 		<td><input type="checkbox" name="nsfw" value="1" /></td>
