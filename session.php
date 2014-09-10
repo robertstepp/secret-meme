e8b485a549389d629074b0f51c3d6e547e3eac8e
Robert <robert@vyperinc.ninja>
2014-09-10 03:13:54 -0400

    Update to SSL

--- session.php
+++ session.php
@@ -5,7 +5,7 @@ session_start();
 //Check whether the session variable SESS_MEMBER_ID is present or not
 if(!isset($_SESSION['sess_user_id']) || (trim($_SESSION['sess_user_id']) == '')) {
 	if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == ""){
-		$redirect = "https://".$_SERVER['HTTP_HOST']."/login_page.php";
+		$redirect = "https://www.".$_SERVER['HTTP_HOST']."/login_page.php";
 		header("Location: $redirect");
 	}
 }
