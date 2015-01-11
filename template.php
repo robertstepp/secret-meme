64ee069482be39d1db31740e02a31fe65e945a9e
Robert <robert@vyperinc.ninja>
2015-01-10 20:21:53 -0500

    Test page added

---
+++ template.php
@@ -0,0 +1,9 @@
+<?php
+// Page Title //
+$title="Bookmark Home";
+// Page Description //
+$Description="Home Bookmark Page to allow use at home/work/travel.";
+require 'db_connect.php'; // Database Connection //
+require 'head.php'; // Head portion of HTML //
+require 'footer.php'; // Footer portion of HTML //	
+?>
--- test.php
+++ test.php
@@ -1,14 +1,32 @@
 <?php
-require 'db_connect.php';
-$query = "SELECT * FROM bookmarks;";
-$result = mysqli_query($con,$query);
-while($row = mysqli_fetch_array($result))	{
-	echo $row['uid'] . " -- " . $row['title'] . " -- " . $row['url'] . "<br />";
+// Create Connection
+$con=mysqli_connect("localhost","root","","bookmarks");
+// Check Connection
+if (mysqli_connect_errno())    {
+    echo "Failed to connect to MySQL: " / mysqli_connect_error();
 }
-echo $_SERVER['HTTPS'];
-if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == ""){
-    $redirect = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-    header("Location: $redirect");
-	echo $redirect;
+$columns = 6;
+$result = mysqli_query($con,"SELECT COUNT(DISTINCT groupid) as countrows from bookmarks.bookmarks where uid=1 AND type='Personal'");
+$types = mysqli_query($con,"SELECT `groupID` AS `QgroupID`, 'uid' FROM `bookmarks`.`bookmarks` AS `bookmarks` WHERE type='Personal' AND uid='1' AND nsfw='1' OR type='Personal' AND uid='1' AND nsfw='0' GROUP BY `groupID` ORDER BY `QgroupID` ASC");
+if($result)
+	{
+	while($splitrows=mysqli_fetch_row($result)){
+		$rows = ceil($splitrows[0]/$columns);
+		$total = $splitrows[0];
+	}
 }
+echo $total;
+$t=1;
+	for ($c=1;$c<=$columns;$c++){
+		echo '<table style="float:left;" id="table'.$c.'">';
+		for ($i=1;$i<=$rows;$i++){
+			$row = mysqli_fetch_array($types);
+			echo '<tr><td>'.$row['QgroupID'].'<br></td></tr>';
+			$t++;
+			if($t>$total){
+				break;
+			}			
+		}
+		echo '</table>';
+	}
 ?>
