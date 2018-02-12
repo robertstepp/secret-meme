25744051e02d52d57e7f316f1ff9fdb23fc3163b
Robert Stepp <robert@vyperinc.ninja>
2018-02-11 19:34:48 -0800

    asd

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,13 @@
 <?php
 // Create Connection
-$con=mysqli_connect("localhost","root","","bookmarks");
+$dbname = 'localhost:3307';
+$dbuser = 'username';
+$dbpass = 'password';
+$dbhost = 'location';
+$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to connect to '$dbhost'");
+mysql_select_db($dbname) or die("Could not open the database '$dbname'");
 // Check Connection
-if (mysqli_connect_errno())    {
+if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " / mysqli_connect_error();
 }
 ?>
